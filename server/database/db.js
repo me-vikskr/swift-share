2d8ec6be947e4d441e589238bda504380effffb6
me-vikskr <vikashkr.bes33@gmail.com>
2023-08-02 13:30:36 +0530

    Updated

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const DBConnection = async () => {
-    const MONGODB_URL = `mongodb://127.0.0.1:27017/file-sharing`;
+    const MONGODB_URL = `mongodb+srv://resume-builder:dPc6iZekRjL00sAQ@swift-share.wc4zue8.mongodb.net/?retryWrites=true&w=majority`;
     try{
         await mongoose.connect(MONGODB_URL, {
             useNewUrlParser: true,
